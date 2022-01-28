4e01232900b80178083e22490414a7934f02b269
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-01-28 19:41:15 +0900

    20220128

--- 競技プログラミング典型/競プロ典型-11-2.py
+++ 競技プログラミング典型/競プロ典型-11-2.py
@@ -1,11 +1,13 @@
-from collections import defaultdict
 N=int(input())
-dp=[defaultdict(list) for _ in range(N)]
+dp=[0]*5001
 D=[0]*N;C=[0]*N;S=[0]*N
 for i in range(N):
     D[i],C[i],S[i]=map(int,input().split())
 zip_list=zip(D,C,S)
 zip_sort=sorted(zip_list)
 D,C,S=zip(*zip_sort)
-if C[0]>D[0]:dp[0][0]=0
-else:dp[0][C[0]]=S[0]
+for i in range(N):
+    if D[i]<C[i]:continue
+    for j in range(D[i]-C[i],-1,-1):
+        dp[j+C[i]]=max(dp[j+C[i]],dp[j]+S[i])
+print(max(dp))
