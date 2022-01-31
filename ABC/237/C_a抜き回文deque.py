233deee5ea03731da66759f993c5fe61c46a29d2
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-01-31 21:35:55 +0900

    20220131

--- ABC/237/C_a抜き回文deque.py
+++ ABC/237/C_a抜き回文deque.py
@@ -1,14 +1,19 @@
-from collections import deque
-S=deque(list(input()))
-while S[0]=='a':
-    S.popleft()
-    if len(S)==0:
+S=list(input())
+l=0
+r=len(S)
+while S[l]=='a':
+    l+=1
+    if l==r:
         print('Yes')
         exit()
-while S[-1]=='a':
-    S.pop()
+r-=1
+while S[r]=='a':
+    r-=1
+if l>(len(S)-r-1):
+    print('No')
+    exit()
 flag=True
-l=len(S)-1
-for i in range(len(S)//2):
-    if S[i]!=S[l-i]:flag=False
+for i in range(l,r):
+    if S[i]!=S[r]:flag=False
+    r-=1
 print('Yes' if flag else 'No')
