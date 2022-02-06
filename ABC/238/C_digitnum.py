6d95161204bd06ee89b0c26d5fde4ba77292b3e3
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-02-06 14:51:28 +0900

    20220206

--- ABC/238/C_digitnum.py
+++ ABC/238/C_digitnum.py
@@ -1,17 +1,17 @@
-a=[[0]*10 for _ in range(17)]
-for i in range(17):
-    for j in range(1,10):
-        if j==1:
-            a[i][j]=1
-        elif j==2:
-            if i==0:
-                a[i][j]=3
-            else:
-                a[i][j]=a[i-1][9]+2*10**(i)+1
-        else:
-            if i==0:
-                a[i][j]=a[i][j-1]+j
-            else:
-                a[i][j]=a[i][2]+(10**(i+1))*(j-2)+a[i][j-1]-1
-print(a)
-N=int(input())
+def tousa(n):
+    return (1+n)*n//2
+
+N=list(input())
+l=len(N)
+N=int(''.join(N))
+A=998244353
+sum=0
+for i in range(l-1):
+    k=9*(10**i)
+    sum+=tousa(k)
+    sum%=998244353
+k=10**(l-1)
+N-=k-1
+sum+=tousa(N)
+sum%=998244353
+print(sum)
