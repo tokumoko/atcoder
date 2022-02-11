716414cc84ddaea7cfeda183dd5f0a837b3cee19
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-02-11 18:49:21 +0900

    20220211

--- ABC/test.py
+++ ABC/test.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-m=defaultdict(list)
-m[0]=1
-print(m[0])
-print(len(m[1]))
+mati=[[(1, 2), (2, 3)], [(0, 2), (4, 2)], [(0, 3), (3, 1), (4, 4)], [(2, 1)], [(1, 2), (2, 
+4)]]
+for i,j in mati[1]:
+    print(i,j)
---
+++ 競技プログラミング典型/競プロ典型-13_重み付きグラフ.py
@@ -0,0 +1,24 @@
+import heapq
+def DFS(s):
+    dist=[10**15]*N
+    que=[(0,s)]
+    heapq.heapify(que)
+    while len(que):
+        t,n=heapq.heappop(que)
+        if dist[n]>t:
+            dist[n]=t
+            for i,j in mati[n]:
+                if dist[i]==10**15:
+                    heapq.heappush(que,(dist[n]+j,i))
+    return dist
+
+N,M=map(int,input().split())
+mati=[[] for _ in range(N)]
+for i in range(M):
+    A,B,C=map(int,input().split())
+    A-=1;B-=1
+    mati[A].append((B,C))
+    mati[B].append((A,C))
+ans1=DFS(0)
+ans2=DFS(N-1)
+for i in range(N):print(ans1[i]+ans2[i])
