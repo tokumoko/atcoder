fbfaba65ea4a9bcb8fa6f243fd3b085149b144f4
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-03-04 22:22:16 +0900

    20220304

---
+++ 競技プログラミング典型/競プロ典型-21_SCC.py
@@ -0,0 +1,34 @@
+#強連結成分分解
+import sys
+sys.setrecursionlimit(1e5)
+def dfs(w):
+    t[w]=False
+    for j in g[w]:
+        if t[j]:dfs(j)
+    label.append(w)
+    return
+def ndfs(w,count):
+    t[w]=False
+    for j in ng[w]:
+        if t[j]:count=ndfs(j,count+1)
+    return count
+
+n,m=map(int,input().split())
+g=[[] for _ in range(n+1)]
+ng=[[] for _ in range(n+1)]
+label=[]
+for i in range(m):
+    a,b=map(int,input().split())
+    g[a].append(b)
+    ng[b].append(a)
+t=[True]*(n+1)
+for i in range(1,n+1):
+    if t[i]:dfs(i)
+t=[True]*(n+1)
+ans=0
+label.reverse()
+for i in label:
+    if t[i]:
+        a=ndfs(i,1)
+        ans+=a*(a-1)//2
+print(ans)
