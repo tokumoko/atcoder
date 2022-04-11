5149618bbda7c9532af3fc4d45abb782660ec384
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-04-11 22:19:24 +0900

    20220411

---
+++ C++/競プロ典型-42_9の倍数の性質.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n)    for(long long i = 0; i < n; i++);
+using ll = long long;
+
+int main(){
+    int K;
+    cin >> K;
+    vector<int>(K+1);
+    
+}
