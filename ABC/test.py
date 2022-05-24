65d868c24a828c8804cf3ee2688adf479931b78c
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-05-24 18:34:58 +0900

    20220524

--- ABC/test.py
+++
@@ -1,8 +0,0 @@
-import heapq
-
-
-a = [(2, 12), (32, 2), (3, 3)]
-heapq.heapify(a)
-print(heapq.heappop(a))
-print(heapq.heappop(a))
-print(heapq.heappop(a))
---
+++ アルゴリズム数学/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    n >>= 1
+    r = f(r, r)
+print(ans[1])
