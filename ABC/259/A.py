7b22c56dbadc65f23f6de9cb4efec0c27cc12113
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-07-11 20:36:12 +0900

    20220711

---
+++ ABC/259/A.py
@@ -0,0 +1,2 @@
+n, m, x, t, d = map(int, input().split())
+print(t - d * (x - min(m, x)))
--- ABC/test.py
+++ ABC/test.py
@@ -1,3 +1,3 @@
-for i in range(10):
-    print(i)
-    i += 1
+import numpy as np
+l = np.array[[1, 2, 2], [1, 2, 3], [1, 3, 4]]
+print(l)
