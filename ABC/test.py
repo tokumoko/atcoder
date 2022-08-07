17914b52ccacf1fd4435b0bbc4ce5206e293a792
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-08-07 21:19:02 +0900

    20220807

--- ABC/test.py
+++ ABC/test.py
@@ -1,3 +1,14 @@
-n = int(input())
-a = list(map(int, input().split())
-print(a)
+r = [[] for _ in range(6)]
+r[0] = [-1, 2, 4, 1, 3, -1]
+r[1] = [3, -1, 0, 5, -1, 2]
+r[2] = [1, 5, -1, -1, 0, 4]
+for i in range(3):
+  r[i+3] = 
+print(r)
+dice = list(map(int, input().split()))
+q = int(input())
+for i in range(q):
+  a, b = map(int, input().split())
+  a_ind = dice.index(a)
+  b_ind = dice.index(b)
+  print(dice[r[a_ind][b_ind]])
